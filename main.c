f73447584003524488f9e658f748707044ecc2f6
aedelgado19 <aedelgado19@gmail.com>
2021-05-31 22:54:27 -0700

    initial

---
+++ main.c
@@ -0,0 +1,13 @@
+/* Author: Allison Delgado
+   Class: CS 201 Spring 2021
+   This porgram performs many operations (addition,
+   subtraction, multiplication, etc) on up to 15 arguments.
+   
+   Last updated: May 31, 2021
+ */
+#include <stdio.h>
+
+int main(){
+
+  return 0;
+}
